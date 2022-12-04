26f872a5389c206e329f0e781aa2344113d93ebe
3A932010 <jack37699646@gmail.com>
2022-12-04 15:16:05 +0800

    練習六：更新資料、(修正admin/posts/index.blade.php)

--- database/migrations/2022_12_04_053316_add_is_feature_in_posts_table.php
+++ database/migrations/2022_12_04_053316_add_is_feature_in_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            table->boolean('is_feature')->default(false)->after('content');
+            $table->boolean('is_feature')->default(false)->after('content');
         });
     }
 
